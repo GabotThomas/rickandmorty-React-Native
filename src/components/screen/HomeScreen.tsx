082fc4d94b71e35352fa13ed1acac9536bde8fbb
Fabien <86556152+BastosFab@users.noreply.github.com>
2022-11-07 23:38:42 +0100

    Homepage start creation

--- src/components/screen/HomeScreen.tsx
+++ src/components/screen/HomeScreen.tsx
@@ -2,13 +2,20 @@
 import react from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { StyleSheet, Text } from 'react-native';
-
+import { StyleSheet, Text, Image, View } from 'react-native';
 
 const HomeScreen = ({ navigation }: any) => {
+
+    const titleImg = require('../../assets/imgs/Title.png')
+    const portal = require('../../assets/imgs/RickAndMortyPortal.png')
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.text}>Hello World</Text>
+            <View style={{flex: 1}}>
+                <Image source={titleImg} style={{width: '100%', height: 150, padding: 20}}></Image>
+                <View style={{height: '50%', padding: 20}}>
+                    <Image source={portal} style={{flex: 1, justifyContent:'center' , width: '100%'}}></Image>
+                </View>
+            </View>
         </SafeAreaView>
     );
 }
@@ -16,8 +23,8 @@ const HomeScreen = ({ navigation }: any) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#000',
-        padding: 20
+        // backgroundColor: '#262626',
+        backgroundColor: '#FFF',
     },
     text: {
         color: 'green'
