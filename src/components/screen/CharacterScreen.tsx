3db25e9b23e596b816635cd11e400a3c9b12276b
Fabien <86556152+BastosFab@users.noreply.github.com>
2022-11-16 11:20:00 +0100

    Ajout expandable episode list

--- src/components/screen/CharacterScreen.tsx
+++ src/components/screen/CharacterScreen.tsx
@@ -1,14 +1,36 @@
-import react from 'react';
+import react, {useEffect, useState} from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { FlatList, StyleSheet, Text, View, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Image, Dimensions, TouchableOpacity, Animated } from 'react-native';
 import useGetCharacter from '../../hooks/useGetCharacter';
 
 const CharacterScreen = ({ route, navigation }: any) => {
     const { id } = route.params;
     const [character, loading, error] = useGetCharacter({ id });
 
-    return (
+    const ExpandableView = ({ expanded = false }) => {
+        const [height] = useState(new Animated.Value(0));
+
+        useEffect(() => {
+            Animated.timing(height, {
+              toValue: !expanded ? 200 : 0,
+              duration: 150,
+              useNativeDriver: false
+            }).start();
+          }, [expanded, height]);
+
+          return (
+            <Animated.View style={{ height, backgroundColor: "#FAFAFA", borderRadius: 10, paddingVertical: 10, paddingHorizontal: 20 }}>
+                {character?.episode.length > 0 &&
+                <FlatList data={character?.episode} renderItem={({ item }) => <EpisodeCard episode={item} />} />
+            }
+            </Animated.View>
+          );
+    };
+    
+    const [isExpanded, setIsExpanded] = useState(true);
+
+    return (    
         <SafeAreaView style={styles.container}>
             <View style={{ flex: 1 }}>
                 <View style={{ flex: 1, flexDirection: 'row' }}>
@@ -30,9 +52,8 @@ const CharacterScreen = ({ route, navigation }: any) => {
                     </View>
                 </View>
                 <View style={{ flex: 2 }}>
-                    {character?.episode.length > 0 &&
-                        <FlatList data={character?.episode} renderItem={({ item }) => <EpisodeCard episode={item} />} />
-                    }
+                    <Text style={[styles.textTitle, styles.margin]} onPress={() => {setIsExpanded(!isExpanded)}}>Episodes</Text>
+                    <ExpandableView expanded={isExpanded}/>
                 </View>
             </View>
         </SafeAreaView>
@@ -40,7 +61,9 @@ const CharacterScreen = ({ route, navigation }: any) => {
 }
 
 const EpisodeCard = ({ episode }) => {
-    return (<Text style={styles.text}>{episode.name}</Text>)
+    return (
+        <Text style={{color: "#000", marginVertical: 4, fontWeight: 'bold'}}>{episode.name} - {episode.episode}</Text>
+    )
 }
 
 const styles = StyleSheet.create({
@@ -68,6 +91,10 @@ const styles = StyleSheet.create({
         height: 15,
         borderRadius: 50,
         marginRight: 5
+    },
+    margin: {
+        marginTop: 40,
+        marginBottom: 10
     },
     'Alive': {
         backgroundColor: 'green'
